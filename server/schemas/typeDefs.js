35385b48d6da8c6e1d70b325087d467500e43c81
Fmquito45 <fmquitox45@gmail.com>
2023-10-28 00:20:55 -0400

    defines the query and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,6 +23,17 @@ type Auth {
     token: ID!
     user: User
   }
+
+type Query {
+    me: User  
+  }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+}
 `;
 
 // export the typeDefs
