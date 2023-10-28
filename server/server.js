6ba9801998b0b23fb370eaeac99bd97f91dc78fb
Fmquito45 <fmquitox45@gmail.com>
2023-10-27 23:45:27 -0400

    add server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+
+const { ApolloServer } = require('@apollo/server');
+const { authMiddleWare }  = require('./utils/auth.js')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer ({
+  typeDefs,
+  resolvers,
+  context: authMiddleWare,
+})
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const startApolloServer = async () => {
+  await server.start();
 
-app.use(routes);
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+   
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    
+    app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+  
+  // Important for MERN Setup: Any client-side requests that begin with '/graphql' will be handled by our Apollo Server
+  app.use('/graphql', expressMiddleware(server));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
+
