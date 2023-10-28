008fa0b16ee7cf10456b95dd65afa0ba0d5622db
Fmquito45 <fmquitox45@gmail.com>
2023-10-28 00:25:13 -0400

    defines mutation in resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,7 +11,54 @@ const resolvers = {
             throw new AuthenticationError;
         },
     },
-   
+    Mutation: {
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, {authors, bookId, description, image, link, title}, context) => {
+            if (context.user) {
+                description = description || '';
+                return await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: {authors, bookId, description, image, link, title} } },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError;
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                return await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: {bookId }} },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError;
+        },
+    }
 };
 
 module.exports = resolvers;
