3d73ea11bbbd538bb1b7af5bfc6b322a1836c3f7
Fmquito45 <fmquitox45@gmail.com>
2023-10-28 15:22:46 -0400

    adds imports to signupfrom

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutattions';
 
 const SignupForm = () => {
   // set initial form state
